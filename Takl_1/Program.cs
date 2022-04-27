16f0484df042a3e07bd229e6a1e4d21ece5fbd68
Marina <SimashMarinka@gmail.com>
2022-04-27 14:30:54 +0500

    Задачи

---
+++ Takl_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine ("Является ли первое число квадратом второго?");
+Console.Write("Введите число a -> ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b -> ");
+int numB = int.Parse(Console.ReadLine());
+// Console.Write("Квадрат числа равен:");
+int numResult = numA * numA;
+if (numResult == numB)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
