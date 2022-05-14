b76e6179f352655745bc71b9495767e8637822dc
Marina <SimashMarinka@gmail.com>
2022-05-15 00:32:07 +0500

    Task_12 исправления

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -1,7 +1,11 @@
-﻿int num1 = 20; //new Random().Next(10,100);
-int num2 = 47; //new Random().Next(10,100);
-Console.WriteLine($"Первое число = {num1}"); 
-Console.WriteLine($"Второе число = {num2}");
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+Если 2 не кратно числу, то программа выводит остаток от деления.*/
+
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
 int num = num1 % num2;
 if (num1 > num2)
 {
