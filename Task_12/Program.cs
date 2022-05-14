ca4d0782ae016e96648ffacdb490f38dfb8f0942
Marina <SimashMarinka@gmail.com>
2022-05-15 00:02:46 +0500

    Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,20 @@
+﻿int num1 = 20; //new Random().Next(10,100);
+int num2 = 47; //new Random().Next(10,100);
+Console.WriteLine($"Первое число = {num1}"); 
+Console.WriteLine($"Второе число = {num2}");
+int num = num1 % num2;
+if (num1 > num2)
+{
+    if (num == 0) 
+    {
+        Console.WriteLine("Число кратно");
+    }
+    else
+    {
+        Console.WriteLine($"Не кратно, остаток = {num}");
+    }
+}
+else 
+{
+    Console.WriteLine("Число не делится");
+}
