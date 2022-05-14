2130e3c0088265b44b95cf3d57b77ae0c362279c
Marina <SimashMarinka@gmail.com>
2022-05-14 22:34:17 +0500

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine(number);
+int num = number / 10;
+int numb = number % 10;
+Console.WriteLine($"Первое число = {num}");
+Console.WriteLine($"Второе число = {numb}");
+Console.Write("Максимальное число = ");
+Console.WriteLine(num > numb ? num : numb); //if (num>numb) max = num; else max = numb;
