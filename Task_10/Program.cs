dc89d40467100bdf1e2a67758d9d730cf7a83d91
Marina <SimashMarinka@gmail.com>
2022-05-16 19:49:51 +0500

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число,
+// а на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int num1 = number / 10;
+int result = num1 % 10;
+
+Console.WriteLine(result);
