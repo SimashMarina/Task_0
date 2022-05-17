305cdfdccba4fa8518724412b87e23679279e3b8
Marina <SimashMarinka@gmail.com>
2022-05-17 20:55:40 +0500

    Task_15_array

---
+++ Task_15_array/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15 решение через массив.
+
+Console.Write("Введите число дня недели от 1 до 7: ");
+var day = Convert.ToInt32(Console.ReadLine());
+string[] isWeekend = {"Нет", "Нет", "Нет", "Нет", "Нет", "Да", "Да"};
+if(day > 0 && day < 8)
+{
+    Console.WriteLine(isWeekend[day - 1]);
+}
+else Console.WriteLine("Вы ввели некорректное значение!");
