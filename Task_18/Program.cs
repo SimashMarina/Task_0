bc946645d64a757a32d54fe3024f49dc16c9f94e
Marina <SimashMarinka@gmail.com>
2022-05-17 22:37:24 +0500

    Task_18 and Nask_21

---
+++ Task_18/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+var n = (Console.ReadLine());
+
+if (n == "1")
+{
+    Console.WriteLine("x > 0; Y > 0"); 
+}
+else if (n == "2")
+{
+    Console.WriteLine("x < 0; Y > 0");
+}
+else if (n == "3")
+{
+    Console.WriteLine("x < 0; Y < 0"); 
+}
+else if (n == "4")
+{
+    Console.WriteLine("x > 0; Y < 0"); 
+}
+else Console.WriteLine("Вы ввели некорректное значение");
---
+++ Task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координату A: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату B");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+int num = x1 - x2;
+int num1 = y1 - y2;
+
+double result = Math.Sqrt(num * num + num1 * num1);
+Console.WriteLine($"Расстояние от первой точки до второй: {result}");
