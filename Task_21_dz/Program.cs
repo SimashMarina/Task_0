a2b9305f6884fa7c467e71a8c424d7f4d57f701b
Marina <SimashMarinka@gmail.com>
2022-05-22 12:45:55 +0500

    Task_21_dz

---
+++ Task_21_dz/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве 
+A(3,6,8); B (2,1,-7) -> 15,84 */
+Console.WriteLine("Введите координату A: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату B");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+int num = x1 - x2;
+int num1 = y1 - y2;
+int num2 = z1 - z2;
+
+double result = Math.Sqrt(num * num + num1 * num1 + num2 * num2);
+double n = Math.Round(result, 2);
+Console.WriteLine($"Расстояние между точками: {n}");
---
+++ Task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int n = 1;
+while (n <= num)
+{
+    Console.WriteLine($"Число{n, 5}|Куб{n*n*n, 5}");
+    n++;
+}
